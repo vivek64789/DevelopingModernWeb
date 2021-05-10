0e96b6ec7e6ea75efc2c92213539dd1604e93e9c
Bibekanand Kushwaha <60493525+vivek64789@users.noreply.github.com>
2021-05-10 19:48:45 +0545

    Update settings.py

--- EntrancPreparation/settings.py
+++ EntrancPreparation/settings.py
@@ -98,18 +98,12 @@ WSGI_APPLICATION = 'EntrancPreparation.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'entrancepreparation',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
 
 
 # Password validation
